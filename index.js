08d5daf316db183742b0c2fa85ac21c13a7237ee
Pintu-Roy121 <roypintu121@gmail.com>
2022-11-24 14:10:54 +0600

    products get

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -11,7 +12,48 @@ app.use(cors());
 app.use(express());
 
 
+// Database connection........................
 
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.geiv5ao.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        const categoriesCollections = client.db('BikeReseller').collection('categories');
+        const productsCollections = client.db('BikeReseller').collection('products')
+
+        // Get all bike breand category.............................
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollections.find(query).toArray();
+            res.send(categories);
+        })
+
+        // get category products.................................
+
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const result = await productsCollections.find(query).toArray();
+            res.send(result)
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { category_id: id }
+            const result = await productsCollections.find(query).toArray();
+            res.send(result)
+        })
+
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.error)
 
 
 // basic setup.....................
@@ -21,5 +63,5 @@ app.get('/', async (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Bikereselle is running on port ${port}`);
+    console.log(`server is running on port ${port}`);
 })
